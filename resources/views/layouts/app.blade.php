3822a7962101d94e4513b306b89f1a1c15ae3fed
Purnama Ridzky Nugraha <purnamanugraha492@gmail.com>
2025-08-20 16:01:04 +0700

    fix: Update default page title to 'ALADZAN CORPORA'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>@yield('title', 'ResellerShop')</title>
+    <title>@yield('title', 'ALADZAN CORPORA')</title>
     <link rel="icon" type="image/png" href="{{ asset('storage/logo1.png') }}">
     <link rel="shortcut icon" href="{{ asset('storage/logo1.png') }}">
 
